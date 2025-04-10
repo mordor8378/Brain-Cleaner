2f33b4a6ad70804f6c7a809b8da410b53ef2449b
Jaehyun Yoo <jaehyun.yoo@stonybrook.edu>
2025-04-10 16:22:29 +0900

    fix: entity 수정 및 패키지 복구

--- be/src/main/java/com/dd/blog/domain/post/comment/entity/Comment.java
+++ be/src/main/java/com/dd/blog/domain/post/comment/entity/Comment.java
@@ -1,6 +1,7 @@
 package com.dd.blog.domain.post.comment.entity;
 
 import com.dd.blog.domain.post.post.entity.Post;
+import com.dd.blog.domain.user.user.entity.User;
 import com.dd.blog.global.jpa.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
