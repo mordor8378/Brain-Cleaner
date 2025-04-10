c647287242f04c0b8402da20e5c000b52974fca0
Eunseo Lee <monimoni@ewhain.net>
2025-04-10 15:47:29 +0900

    verification & category

---
+++ be/src/main/java/com/dd/blog/domain/post/category/entity/Category.java
@@ -0,0 +1,27 @@
+package com.dd.blog.domain.post.category.entity;
+
+import com.dd.blog.domain.post.post.entity.Post;
+import com.dd.blog.global.jpa.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name="category")
+@Getter
+@Setter
+@NoArgsConstructor
+// 카테고리 3개라 필요 없음
+// @Builder
+public class Category extends BaseEntity {
+
+//        @Id
+//        @GeneratedValue(strategy = GenerationType.IDENTITY)
+//        @Column(name = "category_id")
+//        private Long categoryId;
+
+    @Column(name = "category_name", nullable = false)
+    private String categoryName; // 인증게시판, 자유게시판, 정보공유게시판
+
+    @OneToMany(mappedBy = "category")
+    private List<Post> posts;
+}
---
+++ be/src/main/java/com/dd/blog/domain/post/verification/entity/Verification.java
@@ -0,0 +1,42 @@
+package com.dd.blog.domain.post.verification.entity;
+
+import com.dd.blog.domain.user.user.entity.User;
+import com.dd.blog.domain.post.post.entity.Post;
+import com.dd.blog.global.jpa.BaseEntity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "verification")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Verification extends BaseEntity {
+
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "verification_id")
+//    private Long verificationId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user; // 인증 요청한 사용자
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private VerificationStatus status; // PENDING, APPROVED, REJECTED
+
+    // @Column(name = "created_at")
+    // private LocalDateTime createdAt;
+
+    // @Column(name = "updated_at")
+    // private LocalDateTime updatedAt;
+}
+
