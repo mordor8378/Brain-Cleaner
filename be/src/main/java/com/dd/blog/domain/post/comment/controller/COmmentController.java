07e08ff960ddda30061aeee3d8274d16b105261b
user <si159521@gmail.com>
2025-04-14 10:16:52 +0900

    refactor - CommentController : 클래스이름 수정(자꾸 COmmentController로 바뀌어있다..?)
    fix
    - Verification : @Builder를 @SuperBuilder로 변경

--- be/src/main/java/com/dd/blog/domain/post/comment/controller/COmmentController.java
+++ be/src/main/java/com/dd/blog/domain/post/comment/controller/COmmentController.java
@@ -1,18 +1,8 @@
 package com.dd.blog.domain.post.comment.controller;
 
-import com.dd.blog.domain.post.comment.dto.CommentRequestDto;
-import com.dd.blog.domain.post.comment.dto.CommentResponseDto;
-import com.dd.blog.domain.post.comment.entity.Comment;
 import com.dd.blog.domain.post.comment.service.CommentService;
-import com.dd.blog.domain.post.post.dto.PostRequestDto;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("comments")
