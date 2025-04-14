4f21f0f067ba43bce03ce5d62eeecade3d2b9313
user <si159521@gmail.com>
2025-04-14 20:54:07 +0900

    [REFACTOR] PointHistoryRepository - Package pageable인지 Pageable인지 확실하지 않아서 일단 주석으로 추가

--- be/src/main/java/com/dd/blog/domain/point/repository/PointHistoryRepository.java
+++ be/src/main/java/com/dd/blog/domain/point/repository/PointHistoryRepository.java
@@ -3,6 +3,7 @@ package com.dd.blog.domain.point.repository;
 import com.dd.blog.domain.point.entity.PointHistory;
 import com.dd.blog.domain.user.user.entity.User;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +11,6 @@ import org.springframework.stereotype.Repository;
 public interface PointHistoryRepository extends JpaRepository<PointHistory, Long> {
 
     Page<PointHistory> findByUserOrderByCreatedAtDesc(User user, Package pageable);
+//Page<PointHistory> findByUserOrderByCreatedAtDesc(User user, Pageable pageable);
+
 }
