b1de0959c5c267798a4192e0eb943a9173272c55
Eunseo Lee <monimoni@ewhain.net>
2025-04-22 15:23:55 +0900

    [FEATURE] 게시판 - 검색 기능 구현 & UI

--- be/src/main/java/com/dd/blog/domain/post/post/repository/PostRepository.java
+++ be/src/main/java/com/dd/blog/domain/post/post/repository/PostRepository.java
@@ -4,6 +4,8 @@ import com.dd.blog.domain.post.post.entity.Post;
 import com.dd.blog.domain.post.category.entity.Category;
 import com.dd.blog.domain.user.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -17,4 +19,12 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
     // 특정 사용자의 게시물 목록 조회
     List<Post> findByUserOrderByCreatedAtDesc(User user);
+
+    // 사용자 정의 JPQL 쿼리: 검색 타입(type)이 'title'이면 제목에서, 'writer'면 작성자 닉네임에서 keyword를 LIKE 검색
+    @Query("SELECT p FROM Post p " +
+            "WHERE (:type = 'title' AND p.title LIKE %:keyword%) " +
+            "   OR (:type = 'writer' AND p.user.nickname LIKE %:keyword%)")
+    List<Post> searchByTypeAndKeyword(@Param("type") String type, @Param("keyword") String keyword);
 }
+
+
