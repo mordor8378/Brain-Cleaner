27d2edd576f8cb37bac1548a9816125751fc3d8f
user <si159521@gmail.com>
2025-04-22 16:03:41 +0900

    [FEAT] CommentService, CommentUpdateResponseDto, ApiV1CommentController - 댓글, 대댓글 CRUD 완료 [REFACTOR] Dto에 @JsonProperty추가(필드명은camel표기법으로 하되 front에서는 snake표기법으로 하기위해), Comment - import lombok.*;로변경, CommentRepositpory - findByParentId필드 주석처리(안쓰임)

--- be/src/main/java/com/dd/blog/domain/post/comment/controller/ApiV1CommentController.java
+++ be/src/main/java/com/dd/blog/domain/post/comment/controller/ApiV1CommentController.java
@@ -2,6 +2,7 @@ package com.dd.blog.domain.post.comment.controller;
 
 import com.dd.blog.domain.post.comment.dto.CommentRequestDto;
 import com.dd.blog.domain.post.comment.dto.CommentResponseDto;
+import com.dd.blog.domain.post.comment.dto.CommentUpdateResponseDto;
 import com.dd.blog.domain.post.comment.service.CommentService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -17,68 +18,70 @@ import java.util.List;
 @RequestMapping("/api/v1/comments")
 @RequiredArgsConstructor
 public class ApiV1CommentController {
-//    private final CommentService commentService;
-//
-//    //특정 게시글의 모든 댓글 조회(대댓글 포함)
-//    @Operation(
-//            summary = "댓글 전체 조회",
-//            description = "게시글 ID를 통해 해당 게시글의 댓글 및 대댓글을 조회합니다.",
-//            responses = {
-//                    @ApiResponse(responseCode = "200", description = "조회 성공"),
-//                    @ApiResponse(responseCode = "404", description = "해당 게시글 없음")
-//            }
-//    )
-//    @GetMapping("/{postId}")
-//    public ResponseEntity<List<CommentResponseDto>> getAllComments(@PathVariable Long postId){
-//        List<CommentResponseDto> comments = commentService.getAllComments(postId);
-//        return ResponseEntity.ok(comments);
-//    }
-//
-//    //댓글, 대댓글 작성
-//    @Operation(
-//            summary = "댓글 또는 대댓글 작성",
-//            description = "게시글 ID를 기반으로 댓글 또는 대댓글을 작성합니다.",
-//            responses = {
-//                    @ApiResponse(responseCode = "201", description = "작성 성공"),
-//                    @ApiResponse(responseCode = "400", description = "잘못된 요청 (내용 누락 등)"),
-//                    @ApiResponse(responseCode = "404", description = "해당 게시글 없음")
-//            }
-//    )
-//    @PostMapping("/{postId}")
-//    public ResponseEntity<CommentResponseDto> writeComment(@PathVariable Long postId, @Valid @RequestBody CommentRequestDto commentRequestDto){
-//        CommentResponseDto postComment = commentService.writeComment(postId, commentRequestDto);
-//        return ResponseEntity.status(HttpStatus.CREATED).body(postComment);
-//    }
-//
-//    //댓글 수정
-//    @Operation(
-//            summary = "댓글 수정",
-//            description = "댓글 ID를 기반으로 기존 댓글을 수정합니다.",
-//            responses = {
-//                    @ApiResponse(responseCode = "200", description = "수정 성공"),
-//                    @ApiResponse(responseCode = "400", description = "잘못된 요청 (내용 누락 등)"),
-//                    @ApiResponse(responseCode = "404", description = "해당 댓글 없음")
-//            }
-//    )
-//    @PutMapping("/{commentId}")
-//    public ResponseEntity<CommentResponseDto> updateComment(@PathVariable Long commentId, @Valid @RequestBody CommentRequestDto commentRequestDto){
-//        CommentResponseDto updateComment = commentService.updateComment(commentId, commentRequestDto);
-//        return ResponseEntity.ok(updateComment);
-//    }
-//
-//    //댓글 삭제
-//    @Operation(
-//            summary = "댓글 삭제",
-//            description = "댓글 ID를 기반으로 댓글을 삭제합니다.",
-//            responses = {
-//                    @ApiResponse(responseCode = "200", description = "삭제 성공"),
-//                    @ApiResponse(responseCode = "404", description = "해당 댓글 없음")
-//            }
-//    )
-//    @DeleteMapping("/{commentId}")
-//    public ResponseEntity<String> deleteComment(@PathVariable Long commentId){
-//        commentService.deleteComment(commentId);
-//        return ResponseEntity.ok("댓글이 삭제되었습니다.");
-//    }
+    private final CommentService commentService;
+
+    //특정 게시글의 모든 댓글 조회(대댓글 포함)
+    @Operation(
+            summary = "댓글 전체 조회",
+            description = "게시글 ID를 통해 해당 게시글의 댓글 및 대댓글을 조회합니다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "조회 성공"),
+                    @ApiResponse(responseCode = "404", description = "해당 게시글 없음")
+            }
+    )
+    @GetMapping("/{postId}")
+    public ResponseEntity<List<CommentResponseDto>> getAllComments(@PathVariable Long postId){
+        List<CommentResponseDto> comments = commentService.getAllComments(postId);
+        return ResponseEntity.ok(comments);
+    }
+
+    //댓글, 대댓글 작성
+    @Operation(
+            summary = "댓글 또는 대댓글 작성",
+            description = "게시글 ID를 기반으로 댓글 또는 대댓글을 작성합니다.",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "작성 성공"),
+                    @ApiResponse(responseCode = "400", description = "잘못된 요청 (내용 누락 등)"),
+                    @ApiResponse(responseCode = "404", description = "해당 게시글 없음")
+            }
+    )
+    @PostMapping("/{postId}")
+    public ResponseEntity<CommentResponseDto> writeComment(@PathVariable Long postId, @RequestParam Long userId,
+                                                           @Valid @RequestBody CommentRequestDto commentRequestDto){
+        CommentResponseDto postComment = commentService.writeComment(postId, commentRequestDto, userId);
+        return ResponseEntity.status(HttpStatus.CREATED).body(postComment);
+    }
+
+    //댓글 수정
+    @Operation(
+            summary = "댓글 수정",
+            description = "댓글 ID를 기반으로 기존 댓글을 수정합니다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "수정 성공"),
+                    @ApiResponse(responseCode = "400", description = "잘못된 요청 (내용 누락 등)"),
+                    @ApiResponse(responseCode = "404", description = "해당 댓글 없음")
+            }
+    )
+    @PutMapping("/{commentId}/{userId}")
+    public ResponseEntity<CommentUpdateResponseDto> updateComment(@PathVariable Long commentId, @RequestParam Long userId,
+                                                                  @Valid @RequestBody CommentRequestDto commentRequestDto){
+        CommentUpdateResponseDto updatedComment = commentService.updateComment(commentId, commentRequestDto, userId);
+        return ResponseEntity.ok(updatedComment);
+    }
+
+    //댓글 삭제
+    @Operation(
+            summary = "댓글 삭제",
+            description = "댓글 ID를 기반으로 댓글을 삭제합니다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "삭제 성공"),
+                    @ApiResponse(responseCode = "404", description = "해당 댓글 없음")
+            }
+    )
+    @DeleteMapping("/{commentId}")
+    public ResponseEntity<String> deleteComment(@PathVariable Long commentId){
+        commentService.deleteComment(commentId);
+        return ResponseEntity.ok("댓글이 삭제되었습니다.");
+    }
 
 }
--- be/src/main/java/com/dd/blog/domain/post/comment/service/CommentService.java
+++ be/src/main/java/com/dd/blog/domain/post/comment/service/CommentService.java
@@ -2,21 +2,92 @@ package com.dd.blog.domain.post.comment.service;
 
 import com.dd.blog.domain.post.comment.dto.CommentRequestDto;
 import com.dd.blog.domain.post.comment.dto.CommentResponseDto;
+import com.dd.blog.domain.post.comment.dto.CommentUpdateResponseDto;
+import com.dd.blog.domain.post.comment.entity.Comment;
+import com.dd.blog.domain.post.comment.repository.CommentRepository;
+import com.dd.blog.domain.post.post.dto.PostResponseDto;
+import com.dd.blog.domain.post.post.entity.Post;
+import com.dd.blog.domain.post.post.repository.PostRepository;
+import com.dd.blog.domain.user.user.entity.User;
+import com.dd.blog.domain.user.user.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 
 import java.util.List;
+import java.util.stream.Collectors;
 
-public abstract class CommentService {
-//    @Transactional(readOnly = true)
-//    public abstract List<CommentResponseDto> getAllComments(Long postId);
-//
-//    @Transactional
-//    public abstract CommentResponseDto writeComment(Long postId, CommentRequestDto commentRequestDto);
-//
-//    @Transactional
-//    public abstract CommentResponseDto updateComment(Long commentId, CommentRequestDto commentRequestDto);
-//
-//    @Transactional
-//    public abstract void deleteComment(Long commentId);
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+    private final PostRepository postRepository;
+    private final CommentRepository commentRepository;
+    private final UserRepository userRepository;
+
+    @Transactional(readOnly = true)
+    public List<CommentResponseDto> getAllComments(Long postId){
+        // 댓글과 대댓글은 프론트에서 parentId를 활용하여 계층구조처럼 보이게 설정 가능(백엔드에서는 모든댓글을 return)
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new EntityNotFoundException("게시글을 찾을 수 없습니다."));
+
+        List<Comment> comments = commentRepository.findByPostId(postId);
+
+        return comments.stream()
+                .map(CommentResponseDto::fromEntity)
+                .collect(Collectors.toList());
+    };
+
+    @Transactional
+    public CommentResponseDto writeComment(Long postId, CommentRequestDto commentRequestDto, Long userId){
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new EntityNotFoundException("게시글을 찾을 수 없습니다."));
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException("사용자를 찾을 수 없습니다."));
+
+        String content = commentRequestDto.getContent();
+
+        Comment parentComment = null;
+        if (commentRequestDto.getParentId() != null) {
+            parentComment = commentRepository.findById(commentRequestDto.getParentId())
+                    .orElseThrow(() -> new EntityNotFoundException("부모 댓글을 찾을 수 없습니다."));
+        }
+
+        Comment newComment = Comment.builder()
+                .post(post)
+                .user(user)
+                .parent(parentComment)
+                .content(content)
+                .build();
+
+        commentRepository.save(newComment);
+        return CommentResponseDto.fromEntity(newComment);
+    };
+
+    @Transactional
+    public CommentUpdateResponseDto updateComment(Long commentId, CommentRequestDto dto, Long userId) {
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new EntityNotFoundException("댓글을 찾을 수 없습니다."));
+
+        if (!comment.getUser().getId().equals(userId)) {
+            throw new AccessDeniedException("작성자만 수정할 수 있습니다.");
+        }
+
+        comment.setContent(dto.getContent()); // 변경감지
+        return new CommentUpdateResponseDto(comment.getId(), comment.getContent(), comment.getUpdatedAt());
+    }
+
+    @Transactional
+    public void deleteComment(Long commentId){
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new EntityNotFoundException("댓글을 찾을 수 없습니다."));
+
+        // 부모 댓글이 삭제되어도 대댓글들은 남아있게 하기위해 부모관계를 끊음
+        if (comment.getParent() != null) {
+            comment.setParent(null); // 부모 댓글을 null로 설정해서 관계를 끊음
+        }
+        commentRepository.delete(comment);
+    };
 }
