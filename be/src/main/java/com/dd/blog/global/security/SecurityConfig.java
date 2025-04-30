c6779d63a08ad8b1b5f44b0c5ff7e5050612b40e
Jaehyun Yoo <jaehyun.yoo@stonybrook.edu>
2025-04-30 20:11:39 +0900

    [FIX] 백엔드 설정 수정

--- be/src/main/java/com/dd/blog/global/security/SecurityConfig.java
+++ be/src/main/java/com/dd/blog/global/security/SecurityConfig.java
@@ -45,7 +45,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .sessionManagement(sessionManagement ->
