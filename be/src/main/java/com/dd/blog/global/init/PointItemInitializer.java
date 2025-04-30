dff019d3946151d10f5ffedbef28728eeaae7a47
Eunseo Lee <monimoni@ewhain.net>
2025-05-01 00:35:45 +0900

    [feat] added more point items!

--- be/src/main/java/com/dd/blog/global/init/PointItemInitializer.java
+++ be/src/main/java/com/dd/blog/global/init/PointItemInitializer.java
@@ -85,6 +85,153 @@ public class PointItemInitializer implements CommandLineRunner {
                 ":headbang:",
                 "/emojis/headbang.gif"
         );
+        saveIfNotExist(
+                "merongcat",
+                "merongcat",
+                100,
+                ":merongcat:",
+                "/emojis/merongcat.png"
+        );
+        saveIfNotExist(
+                "10-10",
+                "10 out of 10",
+                150,
+                ":ten-ten:",
+                "/emojis/10-outof-10.gif"
+        );
+        saveIfNotExist(
+                "goodluck",
+                "행운의 클로버",
+                150,
+                ":goodluck:",
+                "/emojis/goodluck.gif"
+        );
+        saveIfNotExist(
+                "god",
+                "3대 500 개",
+                100,
+                ":god:",
+                "/emojis/god.png"
+        );
+        saveIfNotExist(
+                "현타",
+                "정신 단디 잡으세요",
+                100,
+                ":feels:",
+                "/emojis/feels.png"
+        );
+        saveIfNotExist(
+                "cool dog",
+                "나는 멋쟁이",
+                200,
+                ":cooldog:",
+                "/emojis/cool-doge.gif"
+        );
+        saveIfNotExist(
+                "박수",
+                "응원의 박수",
+                100,
+                ":clap:",
+                "/emojis/clapping.gif"
+        );
+        saveIfNotExist(
+                "비상등",
+                "조심하세요",
+                150,
+                ":alert:",
+                "/emojis/alert.gif"
+        );
+        saveIfNotExist(
+                "뽀뽀냥이",
+                "쪽!",
+                200,
+                ":bbobbocat:",
+                "/emojis/bbobbocat.jpg"
+        );
+        saveIfNotExist(
+                "맑눈광",
+                "맑은 눈의 광인",
+                150,
+                ":malknun:",
+                "/emojis/malknunguang.jpg"
+        );
+        saveIfNotExist(
+                "sob",
+                "광광",
+                150,
+                ":sob:",
+                "/emojis/sob.png"
+        );
+        saveIfNotExist(
+                "congrats",
+                "심심한 축하의 말씀",
+                100,
+                ":congrats:",
+                "/emojis/tada.png"
+        );
+        saveIfNotExist(
+                "로켓",
+                "발사!",
+                100,
+                ":rocket:",
+                "/emojis/rocket.png"
+        );
+        saveIfNotExist(
+                "two hearts",
+                "하트x2",
+                100,
+                ":twohearts:",
+                "/emojis/two-hearts.gif"
+        );
+        saveIfNotExist(
+                "spinnin' heart",
+                "빙빙 하트",
+                100,
+                ":revolvinghearts:",
+                "/emojis/revolving-hearts.gif"
+        );
+        saveIfNotExist(
+                "cupid heart",
+                "큐피드 하트",
+                100,
+                ":heartwarrow:",
+                "/emojis/heart-with-arrow.gif"
+        );
+        saveIfNotExist(
+                "불타는 하트",
+                "할수있다!!!!",
+                100,
+                ":heartonfire:",
+                "/emojis/heart-on-fire.gif"
+        );
+        saveIfNotExist(
+                "heart beam",
+                "커지는 하트",
+                100,
+                ":growingheart:",
+                "/emojis/growing-heart.gif"
+        );
+        saveIfNotExist(
+                "heart!",
+                "하트!",
+                100,
+                ":heart!:",
+                "/emojis/heart-exclamation.gif"
+        );
+        saveIfNotExist(
+                "따봉",
+                "thumbs-up",
+                50,
+                ":ddabong:",
+                "/emojis/ddabong.gif"
+        );
+        saveIfNotExist(
+                "detoxing",
+                "디톡스 모두모두 화이팅!",
+                100,
+                ":detox:",
+                "/emojis/detoxing.gif"
+        );
     }
 
     private void saveIfNotExist(String name, String desc, int price, String code, String imageUrl) {
